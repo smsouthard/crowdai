b65d90b9d6022a44bdfa4de2e5deb144f7e1c662
Sean Southard <sean.m.southard@gmail.com>
2016-10-22 22:53:36 -0600

    working grey images

--- data_set_maker.py
+++ data_set_maker.py
@@ -1,8 +1,17 @@
 #!usr/bin/env ipython2
 
 import cv2 as cv
+import glob
+
+def greyscale_tifs(image_dir, new_dir):
+  for mask in glob.glob(image_dir + '*.tif'):
+    gmask = cv.imread(mask, cv.IMREAD_GRAYSCALE)
+    _, gmask = cv.threshold(gmask, 0, 1, cv.THRESH_BINARY)
+    cv.imwrite(new_dir + mask.split('/')[-1], gmask )
 
 
+def main():
+  greyscale_tifs( './mask/', './binary_mask/')
 
 if __name__ == '__main__':
-  
+  main()
