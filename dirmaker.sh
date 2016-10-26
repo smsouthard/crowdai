a2b840018875f5b4a4e02278dce342ea43429e2f
Sean Southard <sean.m.southard@gmail.com>
2016-10-26 14:11:41 -0600

    dirmaker

---
+++ dirmaker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir ./cv;
+mkdir ./cv/binary_mask;
+mkdir ./cv/tiles;
+
+mkdir ./train;
+mkdir ./train/binary_mask;
+mkdir ./train/tiles;
+
+mkdir ./test;
+mkdir ./test/binary_mask;
+mkdir ./test/tiles;
